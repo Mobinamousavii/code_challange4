64dd709b3ff7b2cb38badc9ed9c83cce73854eae
mhosseinkaka <mhosseinkaka@gmail.com>
2025-03-07 13:11:48 +0330

    V03

--- alibaba/detail/models.py
+++ alibaba/detail/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from alibaba.airplane.models import Airplane
+from alibaba.bus.models import Bus
+from alibaba.train.models import Train
 
-# Create your models here.
+class Passenger(models.Model):
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=20)
+    national_id = models.CharField(max_length=11, unique=True)
+    email = models.EmailField(null=True, blank=True)
+
+
+class Ticket_bus(models.Model):
+    vehicle = models.ForeignKey(to=Bus, on_delete=models.CASCADE)
+    origin = models.CharField(max_length=100)
+    destination = models.CharField(max_length=100)
+
+
+class Ticket_Airplane(models.Model):
+    vehicle = models.ForeignKey(to=Airplane, on_delete=models.CASCADE)
+    origin = models.CharField(max_length=100)
+    destination = models.CharField(max_length=100)
+
+
+class Ticket_train(models.Model):
+    vehicle = models.ForeignKey(to=Train, on_delete=models.CASCADE)
+    origin = models.CharField(max_length=100)
+    destination = models.CharField(max_length=100)
+
+class Reserve(models.Model):
+    passenger = models.ForeignKey(to=Passenger, on_delete=models.CASCADE, related_name="reserve_passenger")
+    ticket_bus = models.ForeignKey(to=Ticket_bus, on_delete=models.CASCADE, related_name="reserve_passenger")
+    ticket_airplane = models.ForeignKey(to=Ticket_Airplane, on_delete=models.CASCADE, related_name="reserve_passenger")
+    ticket_train = models.ForeignKey(to=Ticket_train, on_delete=models.CASCADE, related_name="reserve_passenger")
